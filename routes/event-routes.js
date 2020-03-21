a2e74c9707d8effa445f96349a391e46cac47526
Alexandre Florindo <alexsimao178@gmail.com>
2020-03-21 13:02:40 +0000

    heroku done

--- routes/event-routes.js
+++ routes/event-routes.js
@@ -13,7 +13,7 @@ const fbase_cert = {
   type: process.env.TYPE,
   project_id: process.env.PROJECT_ID,
   private_key_id: process.env.PRIVATE_KEY_ID,
-  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+  private_key: process.env.PRIVATE_KEY,
   client_email: process.env.CLIENT_EMAIL,
   client_id: process.env.CLIENT_ID,
   auth_uri: process.env.AUTH_URI,
