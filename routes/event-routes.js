896ad1c721827ed6d1f1205284c981f6e1faffea
Alexandre Florindo <alexsimao178@gmail.com>
2020-03-21 12:01:49 +0000

    heroku done

--- routes/event-routes.js
+++ routes/event-routes.js
@@ -6,10 +6,22 @@ const Event = require("../models/event-model");
 
 const admin = require("firebase-admin");
 
-const serviceAccount = require("../configs/fbServiceAccountKey.json");
+
+const fbase_cert = {
+  type: process.env.TYPE ,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY,
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVEIDER_x509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_x509_CERT_URL,
+}
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(fbase_cert),
   databaseURL: "https://bookit-ad3fd.firebaseio.com"
 });
 
