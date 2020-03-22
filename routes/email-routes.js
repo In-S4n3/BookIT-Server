e9684111b0351e24c7a9bab5ffeee7547509b6b7
Alexandre Florindo <alexsimao178@gmail.com>
2020-03-22 23:45:54 +0000

    aws and heroku complete - missing auth for the invitation

--- routes/email-routes.js
+++ routes/email-routes.js
@@ -21,24 +21,26 @@ router.post("/sendEmail", (req, res) => {
 
   nodemailer.createTestAccount((err, account) => {
     const htmlEmail = `
-    <img src="/public/images/logo.png" alt="BookIT logo"/>
+    <img src="/images/logo.png" alt="BookIT logo"/>
     <p>
       Hi ${req.body.eName}! <br/>
       It would be a pleasure if you accept my invition for the event below. <br/>
-      Please, follow the <a href=#>link</a> to confirm your presence!
+      
     </p>
-    <h5>Name of the Event<h5/>
+    <h4>Name of the Event<h5/>
     <p>${req.body.name}</p>
-    <h5>Date of the Event<h5/>
+    <h4>Date of the Event<h5/>
     <p>${req.body.date}</p>
-    <h5>Time of the Event<h5/>
+    <h4>Time of the Event<h5/>
     <p>${req.body.hour}</p>
-    <h5>Name of the restaurant<h5/>
+    <h4>Name of the restaurant<h5/>
     <p>${req.body.restaurantName}</p>
-    <h5>Address of the restaurant<h5/>
+    <h4>Address of the restaurant<h5/>
     <p>${req.body.restaurantAddress}</p>
     <br/>
     <p>${req.body.message}</p>
+
+    Please, check the <a href="http://book-it-ironhack-2020.s3-website.eu-west-3.amazonaws.com/">Event</a> to confirm your presence!
     `;
 
     const transporter = nodemailer.createTransport({
