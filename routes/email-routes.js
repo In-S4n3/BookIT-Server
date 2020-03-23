f66101e482cf81da91740b3be7677276f8b46e2c
Alexandre Florindo <alexsimao178@gmail.com>
2020-03-23 20:01:35 +0000

    Invitation auth

--- routes/email-routes.js
+++ routes/email-routes.js
@@ -44,7 +44,7 @@ router.post("/sendEmail", (req, res) => {
 
     <h4>ESTA DIFERENTE</h4>
 
-    Please, check the <a href="http://book-it-ironhack-2020.s3-website.eu-west-3.amazonaws.com/events/${req.body.event_id}">Event</a> to confirm your presence!
+    Please, check the <a href="http://book-it-ironhack-2020.s3-website.eu-west-3.amazonaws.com/events/${event_id}">Event</a> to confirm your presence!
     `;
 
     const transporter = nodemailer.createTransport({
